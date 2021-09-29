b128571ce69ba53d3aa65788b173aedd1eb50c4a
JanaKudlickova <jana-kudlickova_student2021@wilder.school>
2021-09-29 21:28:53 +0200

    commiting all cow-say files

---
+++ index.js
@@ -0,0 +1,10 @@
+const workshop = require("./information.js");
+console.log(workshop);
+
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm in a good mood!",
+    e : "oO",
+    T : "U "
+}));
---
+++ information.js
@@ -0,0 +1,9 @@
+const myObject = {
+    name: "Jana",
+    campus: "remote-english",
+}
+
+module.exports = {
+    name: "Jana",
+    campus: "remote-english",
+}
